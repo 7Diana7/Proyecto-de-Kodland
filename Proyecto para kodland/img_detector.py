58da18f2ea21dc0788b1bac622f4176ea4edacb9
7Diana7 <134160185+7Diana7@users.noreply.github.com>
2023-12-09 14:14:30 -0300

    Update img_detector.py

--- Proyecto para kodland/img_detector.py
+++ Proyecto para kodland/img_detector.py
@@ -1,31 +1,26 @@
 from flask import Flask,request, send_file
-from imageai.Detection import ObjectDetection
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-from PIL import Image, ImageOps
-import numpy as np
 import os
 import Deteccion as detections
 app = Flask(__name__)
 Deteccion_objetos = detections
+carpeta_destino = 'img'
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
 
-file = request.files['file']
-filename = secure_filename(file.filename)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.run('upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        scanned_path = Deteccion_objetos(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        scanned_path = Deteccion_objetos(file_path)
     return send_file(scanned_path, as_attachment = True)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
